b79632101cd3febb3a8ea22a9be6eeef93449c3d
Abdelrahman Farah Mohammed <abdelrahman.mohammed@bld.ai>
2023-12-19 10:34:44 +0200

    feat: check drone battery level for a given drone

--- src/main/java/com/elmenus/drones/controller/DroneController.java
+++ src/main/java/com/elmenus/drones/controller/DroneController.java
@@ -11,10 +11,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @RestController
 @RequestMapping("/drones")
@@ -41,16 +38,26 @@ public class DroneController {
             var messages = ValidationErrorResponse.serializeErrorResult(bindingResult);
             return ResponseEntity.badRequest().body(messages);
         }
+        if (drone.getState() == DroneState.LOADING && drone.getBatteryCapacity() < 25){
+            var map = new HashMap<>();
+            map.put("state", "Can not register a drone with LOADING state if the battery level is below 25%");
+            return ResponseEntity.badRequest().body(map);
+        }
 
         droneService.save(drone);
         return ResponseEntity.ok(drone);
     }
 
-
+    @GetMapping("/available")
+    public ResponseEntity<?> getAvailableDrones (){
+        List<Drone> ready = new ArrayList<>();
+        ready.addAll(droneService.getAllByState(DroneState.IDLE));
+        ready.addAll(droneService.getAllByState(DroneState.LOADING));
+        return ResponseEntity.ok(ready);
+    }
 
     @GetMapping("/{drone_sn}")
     public ResponseEntity<?> getDroneInfo (@PathVariable("drone_sn") String drone_sn){
-        System.out.println(1);
         Drone drone = droneService.findById(drone_sn);
         if(drone == null){
             return ResponseEntity.badRequest().body("You entered Invalid drone serial number");
@@ -58,14 +65,16 @@ public class DroneController {
         return ResponseEntity.ok(drone);
     }
 
-    @GetMapping("/available")
-    public ResponseEntity<?> getAvailableDrones (){
-        System.out.println(2);
-        List<Drone> ready = new ArrayList<>();
-        ready.addAll(droneService.getAllByState(DroneState.IDLE));
-        ready.addAll(droneService.getAllByState(DroneState.LOADING));
-        return ResponseEntity.ok(ready);
-    }   
+    @GetMapping("/{drone_sn}/battery")
+    public ResponseEntity<?> getDroneBattery (@PathVariable("drone_sn") String drone_sn){
+        Drone drone = droneService.findById(drone_sn);
+        if(drone == null){
+            return ResponseEntity.badRequest().body("You entered Invalid drone serial number");
+        }
+        var map = new HashMap<>();
+        map.put("batteryCapacity", drone.getBatteryCapacity());
+        return ResponseEntity.ok(map);
+    }
 
 
     @ExceptionHandler
